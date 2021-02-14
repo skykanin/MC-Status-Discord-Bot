5606eee69eafc5012e7bae9e506b4c1dfa9f4303
skykanin <3789764+skykanin@users.noreply.github.com>
2021-02-14 16:05:07 +0100

    Clean up 4 year old code

--- bot.js
+++ bot.js
@@ -1,56 +1,66 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
+const fs = require("fs");
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
 const client = new Discord.Client();
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-function commandIs(str, msg){
-	return msg.content.toLowerCase().startsWith("!" + str);
-}
+const commandIs = (str, msg) => msg.content.toLowerCase().startsWith("!" + str);
 
-function status(callback, ip) {
-	var ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET', 'https://mcapi.us/server/status?ip='+ip, true);
-      ourRequest.onload = () => {
-		var ourData = JSON.parse(ourRequest.responseText);
-		callback(null, checkStatus(ourData));
-    };
-	ourRequest.onerror = function() {
-  		console.error(ourRequest.statusText);
-	};
-    ourRequest.send();
-}
+const status = (callback, ip) => {
+  const ourRequest = new XMLHttpRequest();
+  ourRequest.open("GET", "https://mcapi.us/server/status?ip=" + ip, true);
+  ourRequest.onload = () => {
+    const ourData = JSON.parse(ourRequest.responseText);
+    callback(null, checkStatus(ourData));
+  };
+  ourRequest.onerror = () => console.error(ourRequest.statusText);
+  ourRequest.send();
+};
 
-function checkStatus(data){
-	if(data.online){
-		return "The MC server is online, players currently online: " + data.players.now + " /" + data.players.max;
-	} else {
-		return "server offline";
-	}
-}
+const checkStatus = (data) =>
+  data.online
+    ? `The MC server is online, players currently online: ${data.players.now}/${data.players.max}`
+    : "server offline";
 
-client.on('ready', () => {
-	console.log('The bot in online');
+client.on("ready", () => {
+  console.log("The bot in online");
 });
 
-client.on('message', message => {
-	var args = message.content.split(/[ ]+/);
-	if(commandIs("hello", message)){
-		message.reply('Hello there');
-	}
-	if(commandIs("status", message)){
-		if(args.length === 1){
-			message.channel.sendMessage('You did not define an argument variable. Usage: `!status [ip]`');
-		} else if (args.length === 2){
-			status((error, result) => {
-				if (error) {
-					message.channel.sendMessage("error!");
-					return;
-				}
-			message.channel.sendMessage(result);
-		}, args[1]);
-		} else {
-			message.channel.sendMessage('You defined too many arguments. Usage: `!status [ip]`');
-		}
-	}
+client.on("message", (message) => {
+  const args = message.content.split(/[ ]+/);
+
+  // Simple ping pong
+  if (commandIs("hello", message)) {
+    message.reply("Hello there");
+  }
+
+  // Responds with the server status and current amount of online players
+  if (commandIs("status", message)) {
+    switch (args.length) {
+      case 1:
+        message.channel.send(
+          "You did not define an argument variable. Usage: `!status [ip]`"
+        );
+        break;
+      case 2:
+        status((error, result) => {
+          if (error) {
+            message.channel.send("error!");
+            return;
+          }
+          message.channel.send(result);
+        }, args[1]);
+        break;
+      default:
+        message.channel.send(
+          "You defined too many arguments. Usage: `!status [ip]`"
+        );
+    }
+  }
 });
 
-client.login('Mjc5OTQ5NjI2NzY3ODM1MTM3.C4DifA.cWQ1bJHzzWfgtry_kZ09PujyreE');
+// parse the client token from the json file
+let rawdata = fs.readFileSync("key.json");
+let key = JSON.parse(rawdata);
+
+client.login(key.token);
